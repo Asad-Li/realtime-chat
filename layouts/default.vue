ff1e99b47b38d92aa23a814e4b61557e44c1adf1
Asad <PS210896@edu.summacollege.nl>
2025-05-13 16:41:30 +0200

    added layout(unfinished)

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mx-5 mt-5">
+    <div class="flex flex-row">
+      <slot />
+    </div>
+    <div class="text-4xl flex flex-row justify-center">
+      <footer>
+        <UButton class="m-1" color="neutral" icon="si:add-fill" variant="subtle"></UButton>
+        <UButton class="m-1" color="neutral" icon="si:remove-fill" variant="subtle"></UButton>
+        <UButton class="m-1" color="neutral" icon="tabler:pencil" variant="subtle"></UButton>
+      </footer>
+      <UInput class="mt-1.5 ml-1 " ></UInput>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+<script setup lang="ts">
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="">
+    ddsadsadsd
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
