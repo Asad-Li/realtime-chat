a9791abb79c0e6ab3751113c8861901f762d3916
Asad <PS210896@edu.summacollege.nl>
2025-05-15 14:33:18 +0200

    removed socket.io, inplemented vueuse WebSocket

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <Connection/>
+
 </template>
 <script>
+import { useWebSocket } from '@vueuse/core'
+
+const { status, data, close } = useWebSocket('ws://localhost:3000/_ws', {
+  heartbeat: {
+    message: 'ping',
+    interval: 1000,
+    pongTimeout: 1000,
+  },
+})
+
 </script>
 
 
